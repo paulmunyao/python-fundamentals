7acdc347a512f5968cb62be3d59a8e3f688819ad
paulmunyao <paulmunyao094@gmail.com>
2022-07-09 07:45:14 +0300

    Documenting code

--- reverse.py
+++ reverse.py
@@ -2,6 +2,7 @@
 # The output is mostly in a straightforward way going down
 # creating a list of integers
 list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+# print(list(reversed(list1)))- For the list to appear as a list
 # using a for loop 
 # for new in reversed(list):
 #  print(new(list))
