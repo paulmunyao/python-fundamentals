30135643d9dc1568a39ffc65027a536fc7a638a3
paulmunyao <paulmunyao094@gmail.com>
2022-07-09 07:23:21 +0300

    Reverse a list

--- reverse.py
+++ reverse.py
@@ -1,9 +1,11 @@
 # Reversing a list using a for loop
 list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
-new_list = []
-for new in list:
-    print(l)
-    # if l == list:
-    new_list.append(l)
-    new_list.reverse()
-    print(new_list)
+# new_list = []
+# for new in list:
+#     print(l)
+#     # if l == list:
+#     new_list.append(l)
+#     new_list.reverse()
+#     print(new_list)
+# list.reverse()
+# print(list)
