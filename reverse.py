a60745b35c7202552ac2610f3714cac15ee39b6e
paulmunyao <paulmunyao094@gmail.com>
2022-07-09 07:35:49 +0300

    Giving an output

--- reverse.py
+++ reverse.py
@@ -1,6 +1,5 @@
 # Reversing a list using the reversed method
+# The output is mostly in a straightforward way going down
 list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
-
 for new in reversed(list):
-
  print(new)
