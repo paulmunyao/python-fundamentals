cb83d75f0e38c5d9ff31c33960a3ce64adbfc3da
paulmunyao <paulmunyao094@gmail.com>
2022-07-09 08:30:13 +0300

    Reversing using a while loop

--- reverse.py
+++ reverse.py
@@ -19,3 +19,13 @@ for new in list1:
     # using insert method to insert the new item into the new list
     new_list.insert(0,new)
     print(new_list)
+
+
+list1= [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+print('Original list:', list1)
+list2=[]
+length=len(list1)
+while length > 0:
+    list2.append(list1[length-1])
+    length = length - 1
+print('Reversed list:', list2)
