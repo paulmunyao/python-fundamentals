38b2011bc21b943fd8011f456ed14b77fcab2c27
paulmunyao <paulmunyao094@gmail.com>
2022-07-09 07:37:32 +0300

    Change name from numbers to integers

--- reverse.py
+++ reverse.py
@@ -1,6 +1,6 @@
 # Reversing a list using the reversed method
 # The output is mostly in a straightforward way going down
-# creating a list of numbers
+# creating a list of integers
 list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
 for new in reversed(list):
  print(new)
