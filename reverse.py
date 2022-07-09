152ef1c03209cee16e8685e94a8ac1e66a3a9f51
paulmunyao <paulmunyao094@gmail.com>
2022-07-09 08:31:30 +0300

    Create a new empty list

--- reverse.py
+++ reverse.py
@@ -22,7 +22,8 @@ for new in list1:
 
 # Create a new list
 list1= [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
-print('Original list:', list1)
+print(list1)
+# Create a new empty list
 list2=[]
 length=len(list1)
 while length > 0:
