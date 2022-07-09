420475b26babba60b23507448b01f2111be0479d
paulmunyao <paulmunyao094@gmail.com>
2022-07-09 08:10:56 +0300

    Using insert method

--- reverse.py
+++ reverse.py
@@ -11,8 +11,11 @@
 # create a list of integers
 list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
 # Print the list
-print(list1)
+# print(list1)
 # Create an empty list
 new_list = []
 # iterating through the original list which had been created initially
 for new in list1:
+    # using insert method to insert the new item into the new list
+    new_list.insert(0,new)
+    print(new_list)
