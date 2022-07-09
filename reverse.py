3db6aa7c08b48b6c4a9c7b74c16e9aedd0f4ec4b
paulmunyao <paulmunyao094@gmail.com>
2022-07-09 07:03:17 +0300

    Appending to a new list

---
+++ reverse.py
@@ -0,0 +1,9 @@
+# Reversing a list using a for loop
+list = [1,2,3,4,5,6,7,8,9,10,11]
+new_list = []
+for l in list:
+    print(l)
+    if l == list:
+        new_list.append()
+        new_list.reverse()
+print(new_list)
