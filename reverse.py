d50576fcc48afd181f3efad2737e1766709ae9ed
paulmunyao <paulmunyao094@gmail.com>
2022-07-09 07:20:01 +0300

    Change the represeantion of list to new from l in the for loop

--- reverse.py
+++ reverse.py
@@ -1,7 +1,7 @@
 # Reversing a list using a for loop
 list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
 new_list = []
-for l in list:
+for new in list:
     print(l)
     # if l == list:
     new_list.append(l)
