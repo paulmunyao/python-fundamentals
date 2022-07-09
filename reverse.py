f088f3de542e75e3a916f4a9957265cf2904c931
paulmunyao <paulmunyao094@gmail.com>
2022-07-09 07:17:18 +0300

    Reverse a list

--- reverse.py
+++ reverse.py
@@ -3,7 +3,7 @@ list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
 new_list = []
 for l in list:
     print(l)
-    if l == list:
-        new_list.append()
-        new_list.reverse()
-        print(new_list)
+    # if l == list:
+    new_list.append(l)
+    new_list.reverse()
+    print(new_list)
