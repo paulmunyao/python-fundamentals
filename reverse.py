36f941bd57bcf232472b64be102c2dbf64be9aae
paulmunyao <paulmunyao094@gmail.com>
2022-07-11 01:14:40 +0300

    Change variable name to user

--- reverse.py
+++ reverse.py
@@ -43,7 +43,7 @@
 # Formula for converting farenheit to degrees (°F − 32) × 5/9 = °C
 # create a variable for temperature input
 print("Hi stranger")
-ask = str(input("Do you want Celsius or Farenheit"))
+user = str(input("Do you want Celsius or Farenheit"))
 temperature = int(input("Kindly input temperature: "))
 # Create a variable for storing data from the input the input will be converted using a formula
 temp = (temperature * 9/5) + 32
