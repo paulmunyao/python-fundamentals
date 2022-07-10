ea1bdeee201510470af8d8643fd560b32aa19616
paulmunyao <paulmunyao094@gmail.com>
2022-07-10 23:49:26 +0300

    Write formula for conversion of temperature

--- reverse.py
+++ reverse.py
@@ -38,7 +38,7 @@
 #     if i % 5==0 and i % 7==0:
 #         print(i)
 
-# Formula = (60°C × 9/5) + 32 = 140°F
+# Formula for converting degrees to farenheit= (°C × 9/5) + 32 = 140°F
 #Formula for converting farenheit to degrees (°F − 32) × 5/9 = °C
 
 # Temperature converter from celcius to farenheit
