486d0f5e770fb5dec38f20960ab72a758162e3dc
paulmunyao <paulmunyao094@gmail.com>
2022-07-10 23:23:24 +0300

    Create a for loop with range

--- reverse.py
+++ reverse.py
@@ -15,19 +15,22 @@ list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
 # Create an empty list
 new_list = []
 # iterating through the original list which had been created initially
-for new in list1:
+# for new in list1:
     # using insert method to insert the new item into the new list
-    new_list.insert(0,new)
-    print(new_list)
+    # new_list.insert(0,new)
+    # print(new_list)
 
 # Create a new list
-list1= [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
-print(list1)
+# list1= [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
 # Create a new empty list
-list2=[]
-length=len(list1)
-while length > 0:
+# list2=[]
+# length=len(list1)
+# while length > 0:
     # add list1 to list2 using append function
-    list2.append(list1[length-1])
-    length = length - 1
-print(list2)
+    # list2.append(list1[length-1])
+    # length = length - 1
+# print(list2)
+
+create a for loop with a range
+for i in range(1500,2700):
+    # if i % 5 and i % 7:
