9d0e43235184159bf2faa44d20546cd0796fa21c
paulmunyao <paulmunyao094@gmail.com>
2022-07-12 22:04:46 +0300

    Binary search in python

--- reverse.py
+++ reverse.py
@@ -54,18 +54,6 @@
 
 
 
-print("Cel or Faren: ")
-convert = str(input(""))
-if convert == Cel:
-    temperature = int(input("Kindly input temperature: "))
-    Cel = (temperature * 9/5) + 32
-    print(Cel)
-elif convert == Farenheit:
-    temperature = int(input("Kindly input temperature: "))
-    Faren = (temperature - 32) * 5/9
-    print(Faren)
-
-
 # print("Enter 1 to change temperature from Fahrenheit to Celsius")
 # print("Enter 2 to change temperature from Celsius to Fahrenheit")
 # ch = int(input(""))
@@ -77,3 +65,71 @@ elif convert == Farenheit:
 #     ut= float(input("Enter Temperature in Celsius: " ))
 #     ct=(1.8*ut)+32
 #     print(ct,"F")
+
+
+# Binary Search in python
+
+
+# def binarySearch(array, x, low, high):
+
+#     if high >= low:
+
+#         mid = low + (high - low)//2
+
+#         # If found at mid, then return it
+#         if array[mid] == x:
+#             return mid
+
+#         # Search the left half
+#         elif array[mid] > x:
+#             return binarySearch(array, x, low, mid-1)
+
+#         # # Search the right half
+#         # else:
+#         #     return binarySearch(array, x, mid + 1, high)
+
+#     # else:
+#     #     return -1
+
+
+# array = [3, 4, 5, 6, 7, 8, 9]
+# x = 4
+
+# result = binarySearch(array, x, 0, len(array)-1)
+
+# if result != -1:
+#     print("Element is present at index " + str(result))
+# else:
+#     print("Not found")
+
+# Binary Search in python
+
+
+def binarySearch(array, x, low, high):
+
+    # Repeat until the pointers low and high meet each other
+    while low <= high:
+
+        mid = low + (high - low)//2
+
+        if array[mid] == x:
+            return mid
+
+        elif array[mid] < x:
+            low = mid + 1
+
+        else:
+            high = mid - 1
+
+    return -1
+
+
+array = [3, 4, 5, 6, 7, 8, 9]
+x = 4
+
+result = binarySearch(array, x, 0, len(array)-1)
+
+if result != -1:
+    print("Element is present at index " + str(result))
+else:
+    print("Not found")
