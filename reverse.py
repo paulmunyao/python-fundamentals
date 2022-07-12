6c58204f4c8800d278fead17abc349a86b5e7814
paulmunyao <paulmunyao094@gmail.com>
2022-07-12 23:21:52 +0300

    Modify code

--- reverse.py
+++ reverse.py
@@ -75,7 +75,7 @@
         # mid = low + (high - low)//2
 
         # If found at mid, then return it
-        if array[mid] == x:
+        # if array[mid] == x:
             return mid
 
         # Search the left half
