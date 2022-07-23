ce1e8d5dd5db257bbf39b4ca62ce4f2b1a5c29a7
paulmunyao <paulmunyao094@gmail.com>
2022-07-23 23:41:43 +0300

    Modify code

--- reverse.py
+++ reverse.py
@@ -1,5 +1,5 @@
 # Reversing a list using the reversed method
-The output is mostly in a straightforward way going down
+# The output is mostly in a straightforward way going down
 creating a list of integers
 list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
 print(list(reversed(list1)))- For the list to appear as a list
