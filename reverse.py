b22be75e129666eb335471a1fc386b6068d61a0f
paulmunyao <paulmunyao094@gmail.com>
2022-07-23 23:43:24 +0300

    Modify code

--- reverse.py
+++ reverse.py
@@ -12,7 +12,7 @@
 # list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
 # Print the list
 # print(list1)
-Create an empty list
+# Create an empty list
 new_list = []
 iterating through the original list which had been created initially
 for new in list1:
