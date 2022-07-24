44a6fa808bf47c7c069b0773428b8298fb8cdb24
paulmunyao <paulmunyao094@gmail.com>
2022-07-24 19:14:34 +0300

    Modify code

--- reverse.py
+++ reverse.py
@@ -97,7 +97,7 @@ print(temp)
 
 # if result != -1:
     # print("Element is present at index " + str(result))
-else:
+# else:
     print("Not found")
 
 Binary Search in python using recursion method
