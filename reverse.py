ea6c7b7bfcda01382c904898424f9cb1657f8d54
paulmunyao <paulmunyao094@gmail.com>
2022-07-24 19:15:38 +0300

    Modify code

--- reverse.py
+++ reverse.py
@@ -113,7 +113,7 @@ print(temp)
 
         # Search the left half
         # elif array[mid] > x:
-            return binarySearch(array, x, low, mid-1)
+            # return binarySearch(array, x, low, mid-1)
 
         Search the right half
         else:
