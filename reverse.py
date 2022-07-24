d9482d03c26ecbadb917ed5b71ba437a271c0ba0
paulmunyao <paulmunyao094@gmail.com>
2022-07-24 19:14:13 +0300

    Modify code

--- reverse.py
+++ reverse.py
@@ -93,7 +93,7 @@ print(temp)
 # array = [3, 4, 5, 6, 7, 8, 9]
 # x = 4
 
-result = binarySearch(array, x, 0, len(array)-1)
+# result = binarySearch(array, x, 0, len(array)-1)
 
 if result != -1:
     print("Element is present at index " + str(result))
