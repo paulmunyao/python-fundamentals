f2732ac804c14d1c8b965e6569cb0fe5cfc37bd6
paulmunyao <paulmunyao094@gmail.com>
2022-07-26 18:15:31 +0300

    add ingredient

--- reverse.py
+++ reverse.py
@@ -70,4 +70,4 @@ print(temp)
 #converting a list to a dictionary
 #create a list of ingredients
  
-list =  ['450 g long grain white rice, preferably basmati', '75 ml vegetable oil', '2 medium onions, chopped','1 medium chicken or 700â€“900 g lamb on the bone cut in pieces','570 ml water, plus 110 ml water','peel of 1 large orange','50 g sugar', '50 g blanched and flaked almonds','50 g blanched and flaked pistachios', ]
+list =  ['450 g long grain white rice, preferably basmati', '75 ml vegetable oil', '2 medium onions, chopped','1 medium chicken or 700â€“900 g lamb on the bone cut in pieces','570 ml water, plus 110 ml water','peel of 1 large orange','50 g sugar', '50 g blanched and flaked almonds','50 g blanched and flaked pistachios','Â½ tsp saffron or egg yellow food colour  (optional)', ]
